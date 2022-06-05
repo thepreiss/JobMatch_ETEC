e00e5d8949d9ba30374457113592c4af08ee0320
Emerson Reis <teste.eaw@gmail.com>
2022-06-05 20:35:53 -0300

    Initial API REST base

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,42 +1,51 @@
 import React, { useState } from "react";
 import { View, ImageBackground } from "react-native";
 import CardStack, { Card } from "react-native-card-stack-swiper";
-import { City, Filters, CardItem } from "../components";
+import { CardItem } from "../components";
 import styles from "../assets/styles";
+import IMAGE_NOJOBS from "../assets/images/noJobs.jpg";
 import Jobs from "../assets/data/jobs";
 
 const Home = () => {
   const [swiper, setSwiper] = useState<CardStack | null>(null);
 
   return (
-    <ImageBackground
-      source={require("../assets/images/bg.png")}
-      style={styles.bg}
-    >
-      <View style={styles.containerHome}>
-        <View style={styles.top}>
-        </View>
+      <ImageBackground
+        source={require("../assets/images/bg.png")}
+        style={styles.bg}
+      >
+        <View style={styles.containerHome}>
+          <View style={styles.top}>
+          </View>
 
-        <CardStack
-          loop
-          verticalSwipe={false}
-          renderNoMoreCards={() => null}
-          ref={(newSwiper): void => setSwiper(newSwiper)}
-        >
-          {Jobs.map((item) => (
-            <Card key={item.id}>
+          <CardStack
+            //loop
+            verticalSwipe={false}
+            renderNoMoreCards={() => 
+            <Card key="FirstOne">
               <CardItem
-                hasActions
-                image={item.image}
-                name={item.name}
-                description={item.description}
-                matches={item.match}
+                hasActions= {false}
+                name="Não há mais posições disponíveis. Atualize ou volte mais tarde :)"
+                image={IMAGE_NOJOBS}
               />
             </Card>
-          ))}
-        </CardStack>
-      </View>
-    </ImageBackground>
+          }
+            ref={(newSwiper): void => setSwiper(newSwiper)}
+          >
+            {Jobs.map((item) => (
+              <Card key={item.id}>
+                <CardItem
+                  hasActions
+                  image={item.image}
+                  name={item.name}
+                  description={item.description}
+                  matches={item.match}
+                />
+              </Card>
+            ))}
+          </CardStack>
+        </View>
+      </ImageBackground>
   );
 };
 
